2544f32942bc4ccc484215690cccdea1673b9766
Ignas Damunskis <ignas.damunskis@esl-asia.com>
2023-04-01 18:42:37 +0200

    config done

--- cucumber.js
+++ cucumber.js
@@ -2,9 +2,8 @@ module.exports = {
   default: {
     parallel: 2,
     format: ['html:cucumber-report.html'],
-    paths: [
-      'tests/**/*.feature',
-      'tests/**/*.js'
-    ]
+    worldParameters: {
+      baseURL: 'https://als-df-stg.elysium-dfbt.com/m/en'
+    }
   }
 }
---
+++ features/world.js
@@ -0,0 +1,22 @@
+const { setWorldConstructor } = require("@cucumber/cucumber");
+const { webkit, devices } = require('playwright');
+
+class CustomWorld {
+  constructor(options) {
+    this.baseURL = options.parameters.baseURL
+  }
+
+  async openUrl(uri) {
+    const browser = await webkit.launch({
+      // headless: false
+    });
+    const context = await browser.newContext({
+      baseURL: this.baseURL,
+      ...devices['iPhone 12']
+    });
+    this.page = await context.newPage();
+    await this.page.goto(uri);
+  }
+}
+
+setWorldConstructor(CustomWorld);
